66ad7b7bbf45b8779583a0416010f05c622c23c1
ALEXANDER DIONICIO <ALEXANDER-DIONICIO@FALCONEYE.LOCAL>
2019-10-30 17:38:57 -0400

    commit

---
+++ TwitterStreamer/twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that cointain the user credentials to access Twitter Api
+
+ACCESS_TOKEN = "1183769480519999490-VXrkAI67DuASt4ENuiM2wHDmy5niNf"
+ACCESS_TOKEN_SECRET = "D5FyN9Bp3gKrE8Vuh9atDsajJCEsp2MnnGE9oeOvLplxR"
+CONSUMER_KEY = "C1cDH7ARGZQ5C4Zock2Sk5u8i"
+CONSUMER_SECRET = "bcZB6bX68Z8948EWgGD27uwwwtkxx8h3gX7BU4QJNO5sWwYNPl"
